65f940e10735c3239e4752c418d839a08dcda449
Sergio Ayala Hernandez <105652618+SergioAyalaHernandez@users.noreply.github.com>
2025-07-14 18:47:47 -0500

    Update cms.service.ts

--- FRONT_ANGULAR_BASIC_API_MEDICAMENTOS/src/app/cms.service.ts
+++ FRONT_ANGULAR_BASIC_API_MEDICAMENTOS/src/app/cms.service.ts
@@ -10,7 +10,7 @@ export class CmsService {
   constructor(private http: HttpClient) { }
 
   getRegistros(): Observable<any> {
-    return this.http.get('http://3.133.150.85:8080/api/v1/medicamento/');
+    return this.http.get('http://18.225.72.0:8080/api/v1/medicamento/');
   }
 
   agregarRegistro(nombreMedicamento: string, url: string): Observable<any> {
@@ -18,7 +18,7 @@ export class CmsService {
       nombreMedicamento: nombreMedicamento,
       url: url
     };
-    return this.http.post<any>('http://3.133.150.85:8080/api/v1/medicamento/', body);
+    return this.http.post<any>('http://18.225.72.0:8080/api/v1/medicamento/', body);
   }
 
   actualizarRegistro(id: number, nombreMedicamento: string, url: string): Observable<any> {
@@ -26,27 +26,27 @@ export class CmsService {
       nombreMedicamento: nombreMedicamento,
       url: url
     };
-    return this.http.put<any>(`http://3.133.150.85:8080/api/v1/medicamento/${id}`, body);
+    return this.http.put<any>(`http://18.225.72.0:8080/api/v1/medicamento/${id}`, body);
   }
 
   eliminarRegistro(id: number): Observable<any> {
-    return this.http.delete<any>(`http://3.133.150.85:8080/api/v1/medicamento/${id}`);
+    return this.http.delete<any>(`http://18.225.72.05:8080/api/v1/medicamento/${id}`);
   }
 
   getMedicamentoPorId(id: number | undefined): Observable<any> {
-    return this.http.get(`http://3.133.150.851:8080/api/v1/medicamento/byid/${id}`);
+    return this.http.get(`http://18.225.72.0:8080/api/v1/medicamento/byid/${id}`);
   }
 
 
   getRegistrosTwo(): Observable<any> {
-    return this.http.get('http://3.133.150.85:8080/api/eucidiana/tablaA');
+    return this.http.get('http://18.225.72.05:8080/api/eucidiana/tablaA');
   }
 
-  private apiUrl = 'http://3.133.150.85:8080/clasificador/data';
+  private apiUrl = 'http://18.225.72.0:8080/clasificador/data';
 
   enviarValores(valorX: number, valorY: number): Observable<string> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const options = { headers: headers, responseType: 'text' as 'json' };
-    return this.http.post<string>('http://3.133.150.85:8080/clasificador/data', { valorX, valorY }, options);
+    return this.http.post<string>('http://18.225.72.0.85:8080/clasificador/data', { valorX, valorY }, options);
   }
 }
